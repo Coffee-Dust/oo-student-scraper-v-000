9ea278130101c302643a7512d15e2e037d196078
Jonathan Dust <d24dust@hotmail.com>
2018-07-24 22:20:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,9 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash[]
     @name, @location, @twitter, @linkedin, @github, @blog, @profile_quote, @bio, @profile_url = student_hash[:name], student_hash[:location], student_hash[:twitter], student_hash[:linkedin], student_hash[:github], student_hash[:blog], student_hash[:profile_quote], student_hash[:bio], student_hash[:profile_url]
-    
   end
 
   def self.create_from_collection(students_array)
