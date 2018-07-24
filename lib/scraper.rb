14b9dd85aff698b7f3bee07a18a4d8a0e9664f1d
Jonathan Dust <d24dust@hotmail.com>
2018-07-24 20:22:16 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,15 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_url
+    contents = []
+
+
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
